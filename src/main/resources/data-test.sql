986703e2e5b251dae207336bc77f730974464b2a
ByeongeonPark <bang3453@ajou.ac.kr>
2025-05-20 23:11:22 +0900

    gitignore 수정, application-local.properties 분리

---
+++ src/main/resources/data-test.sql
@@ -0,0 +1,20 @@
+INSERT INTO event_banner (banner_order, img_url, site_url, start_date, end_date, is_posted)
+VALUES (1, 'https://picsum.photos/seed/1/600/200', 'https://ajou.ac.kr/page1', '2025-05-20', '2025-06-01', true);
+
+INSERT INTO event_banner (banner_order, img_url, site_url, start_date, end_date, is_posted)
+VALUES (2, 'https://picsum.photos/seed/2/600/200', 'https://ajou.ac.kr/page2', '2025-05-20', '2025-06-01', true);
+
+INSERT INTO event_banner (banner_order, img_url, site_url, start_date, end_date, is_posted)
+VALUES (3, 'https://picsum.photos/seed/3/600/200', 'https://ajou.ac.kr/page3', '2025-05-20', '2025-06-01', true);
+
+INSERT INTO event_banner (banner_order, img_url, site_url, start_date, end_date, is_posted)
+VALUES (4, 'https://picsum.photos/seed/4/600/200', 'https://ajou.ac.kr/page4', '2025-05-20', '2025-06-01', true);
+
+INSERT INTO event_banner (banner_order, img_url, site_url, start_date, end_date, is_posted)
+VALUES (5, 'https://picsum.photos/seed/5/600/200', 'https://ajou.ac.kr/page5', '2025-05-20', '2025-06-01', true);
+
+INSERT INTO event_banner (banner_order, img_url, site_url, start_date, end_date, is_posted)
+VALUES (6, 'https://picsum.photos/seed/6/600/200', 'https://ajou.ac.kr/page6', '2025-05-20', '2025-06-01', true);
+
+INSERT INTO event_banner (banner_order, img_url, site_url, start_date, end_date, is_posted)
+VALUES (7, 'https://picsum.photos/seed/7/600/200', 'https://ajou.ac.kr/page7', '2025-05-20', '2025-06-01', true);
