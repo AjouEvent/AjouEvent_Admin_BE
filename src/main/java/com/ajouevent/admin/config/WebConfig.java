c38c2027edba2b4fe84eff795295ad6618d523f3
ByeongeonPark <bang3453@ajou.ac.kr>
2025-05-27 16:17:19 +0900

    main 브랜치에서 AdminAuthCheckFilter, WebConfig 덮어쓰기

--- src/main/java/com/ajouevent/admin/config/WebConfig.java
+++ src/main/java/com/ajouevent/admin/config/WebConfig.java
@@ -1,6 +1,6 @@
 package com.ajouevent.admin.config;
 
-//import com.ajouevent.admin.auth.AdminAuthCheckFilter;
+import com.ajouevent.admin.auth.AdminAuthCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +11,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    private final AdminAuthCheckFilter adminAuthCheckFilter;
+    private final AdminAuthCheckFilter adminAuthCheckFilter;
 
-//    public WebConfig(AdminAuthCheckFilter adminAuthCheckFilter) {
-//        this.adminAuthCheckFilter = adminAuthCheckFilter;
-//    }
+    public WebConfig(AdminAuthCheckFilter adminAuthCheckFilter) {
+        this.adminAuthCheckFilter = adminAuthCheckFilter;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -28,12 +28,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true); // 쿠키 포함 허용!
     }
-//    @Bean
-//    public FilterRegistrationBean<AdminAuthCheckFilter> adminAuthFilterRegistration() {
-//        FilterRegistrationBean<AdminAuthCheckFilter> registrationBean = new FilterRegistrationBean<>();
-//        registrationBean.setFilter(adminAuthCheckFilter);
-//        registrationBean.addUrlPatterns("/api/admin/*", "/api/admin"); //admin/뒤에 오는 모든 요청은 자동으로 세션 검증을 진행함.
-//        registrationBean.setOrder(1); // 필터 우선순위 지정 (낮을수록 먼저 실행)
-//        return registrationBean;
-//    }
+    @Bean
+    public FilterRegistrationBean<AdminAuthCheckFilter> adminAuthFilterRegistration() {
+        FilterRegistrationBean<AdminAuthCheckFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(adminAuthCheckFilter);
+        registrationBean.addUrlPatterns("/api/admin/*", "/api/admin"); //admin/뒤에 오는 모든 요청은 자동으로 세션 검증을 진행함.
+        registrationBean.setOrder(1); // 필터 우선순위 지정 (낮을수록 먼저 실행)
+        return registrationBean;
+    }
 }
