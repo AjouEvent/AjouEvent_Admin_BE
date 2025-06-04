873266889e95834f92a9b9ef4cfce2ea2a667875
minjoonJ <breauxchristoper@gmail.com>
2025-06-05 05:15:10 +0900

    fix: PATCH 메소드 CORS 설정 추가

--- src/main/java/com/ajouevent/admin/config/WebConfig.java
+++ src/main/java/com/ajouevent/admin/config/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
                 .allowedOrigins("http://localhost:5173") // 프론트 주소 정확히 지정!
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .exposedHeaders("X-Session-Id")
                 .allowCredentials(true); // 쿠키 포함 허용!
